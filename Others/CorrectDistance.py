36773a19fa1dcb4fb3c19c835924521f9e728899
Zhuyonghao <yonghao_zhu@163.com>
2021-04-08 15:13:31 +0800

    update CorrectDistance.py

--- Others/CorrectDistance.py
+++ Others/CorrectDistance.py
@@ -1,23 +1,30 @@
 #! /usr/bin/python3
 # -*- conding=UTF-8 -*-
+
 '''
 1. I can correctly calculate distance between two atoms.
 2. Support only Direct type of POSCAR. 
 3. author: yonghao_zhu@163.com
 '''
-##################################
-import numpy as np
-import linecache, os
-##################################
+
+#############################
+import numpy as np     ######
+import linecache, os   ######
+#############################
+
 '''
 1. input files: POSCAR with Direct mode
 2. Atoms: start 1
 '''
-Distancefun          = {}
-Distancefun['TorF']  = 'T' #T
-Distancefun['Atom1'] = 4
-Distancefun['Atom2'] = 6
-##################################
+
+Distance_fun             = {}
+Distance_fun['TorF']     = 'T'       # T
+Distance_fun['posfile']  = 'XDATCAR' # POSCAR or XDATCAR
+Distance_fun['Atom1']    = 28        # including, start 1
+Distance_fun['Atom2']    = 77        # including, start 1
+Distance_fun['time_ini'] = 1         # including, start 1
+Distance_fun['time_fin'] = 6         # including, start 1
+#########################################################
 def ReadPOSCAR(file='POSCAR',print_='T'):
     '''
     input files: POSCAR
@@ -57,11 +64,15 @@ def ModificationCoordinate(pos):
 
     return pos_new
 
-def Distance(Atom1=1,Atom2=2):
+def Distance(Atom1=1, Atom2=2, pos=[], latt=[], print_='T'):
 
     #read POSCAR and lattice
-    pos_old = ReadPOSCAR(file='POSCAR',print_='F')[2]
-    latt = np.array([[float(m) for m in linecache.getline('POSCAR',i).split()] for i in range(3,6)])
+    if len(pos) == 0: # read position for POSCAR file
+        pos_old = ReadPOSCAR(file='POSCAR',print_='F')[2]
+        latt = np.array([[float(m) for m in linecache.getline('POSCAR',i).split()] for i in range(3,6)])
+    else: # read from XDATCAR
+        pos_old = pos
+
     #test#print(pos_old.shape)
     pos_new = ModificationCoordinate(pos=pos_old)
     #test#print(pos_new.shape)
@@ -83,12 +94,49 @@ def Distance(Atom1=1,Atom2=2):
 
     distance_ = np.linalg.norm(pos_Atom1 - pos_Atom2,axis = 1)# for i in range(pos_Atom1.shape[0]) 
     distance = np.min(distance_)
-    
-    print('###################################')
-    print('Atoms: start 1')
-    print('Distance Atom%s-Atom%s : %.04f' %(Atom1,Atom2,distance),'Å')
-    print('###################################')
-    
-if Distancefun['TorF'] == 'T' and os.path.isfile('POSCAR'):
-    Distance(Atom1=Distancefun['Atom1'],Atom2=Distancefun['Atom2'])
 
+    x = np.where(distance_ == distance)
+    
+    if print_ == 'T':
+        print('###################################')
+        print('Atoms: start 1')
+        print('Distance Atom%s-Atom%s : %.04f' %(Atom1,Atom2,distance),'Å')
+        print('###################################')
+    else:
+        return distance, (pos_new[Atom1-1] + supercell_)[x]
+
+def ReadXDATCAR(Atom1=1, Atom2=2, time_ini=1, time_fin=2):
+
+    seventh_line = linecache.getline('XDATCAR',7).split()
+
+    lattice = np.mat([[float(linecache.getline('XDATCAR',i+1).split()[0]),
+        float(linecache.getline('XDATCAR',i+1).split()[1]),
+        float(linecache.getline('XDATCAR',i+1).split()[2])] for i in range(2,5)])
+
+    comments = ['Direct configuration='];atoms = int(sum([float(i) for i in seventh_line]))
+    for i in range(7):
+        comments.append(linecache.getline('XDATCAR',i+1))
+
+    xdatcar = np.loadtxt('XDATCAR',comments=comments).reshape(-1,atoms,3)[time_ini-1:time_fin,:,:]
+
+    for i in range(time_fin - time_ini + 1):
+        pos = xdatcar[i,:,:]
+        dis = Distance(Atom1=Atom1, Atom2=Atom2, pos=pos, latt=lattice, print_='F')[0]
+        #print(dis)
+
+    return xdatcar, lattice, atoms
+    
+def main():
+    if Distance_fun['TorF'] == 'T':
+        if Distance_fun['posfile'] == 'POSCAR':
+            Distance(Atom1 = Distance_fun['Atom1'],
+                     Atom2 = Distance_fun['Atom2'])
+
+        if Distance_fun['posfile'] == 'XDATCAR':
+            ReadXDATCAR(Atom1    = Distance_fun['Atom1'],
+                        Atom2    = Distance_fun['Atom2'],
+                        time_ini = Distance_fun['time_ini'],
+                        time_fin = Distance_fun['time_fin'])
+
+if __name__ == "__main__":
+	main()
---
+++ Others/StandardDeviations.py
@@ -0,0 +1,130 @@
+#! /usr/bin/python3
+# -*- conding=UTF-8 -*-
+
+'''
+1. I can correctly calculate Standard Deviations(Å).
+2. Support only Direct type of XDATCAR. 
+3. author: yonghao_zhu@163.com
+'''
+
+#############################
+import CorrectDistance as CD
+import numpy as np
+#############################
+
+#############################################
+SD_fun = {}
+SD_fun['TorF'] = True
+#time_ini/time_fin: including, start 1
+#time_ini is standard. 
+SD_fun['time_ini']  = 1
+SD_fun['time_fin']  = 5900
+SD_fun['atom_list'] = [37,    37 ,  148]
+SD_fun['save_avg']  = True  # True or False
+SD_fun['save_time'] = False  # True or False
+#############################################
+
+def PrintOut_avg(atom_list = [37, 148, 37], sd_atoms = np.zeros([1,1])):
+
+	for i in range(len(atom_list)):
+		if i == 0:
+			print(atom_list[i],'--','1 -',atom_list[i])
+			tmp = sd_atoms[: atom_list[i]]
+			avg = sum(tmp) / atom_list[i]
+			print(avg)
+			print('=========================')
+
+		if i > 0:
+			print(atom_list[i],'---',sum(atom_list[:i])+1,'-',sum(atom_list[:i])+atom_list[i])
+			tmp = sd_atoms[sum(atom_list[:i]): sum(atom_list[:i])+atom_list[i]]
+			avg = sum(tmp) / atom_list[i]
+			print(avg)
+			print('=========================')
+
+def SaveOut_time(atom_list = [37, 148, 37], sd_atoms_time = np.zeros([1,1])):
+
+	print('sd_atoms_time.shape=[atoms, times]=',sd_atoms_time.shape)
+
+	for i in range(len(atom_list)):
+		name = '%s.txt' % (i+1)
+		if i == 0:
+			tmp = sd_atoms_time[: atom_list[i], :]
+		if i > 0:
+			tmp = sd_atoms_time[sum(atom_list[:i]): sum(atom_list[:i])+atom_list[i], :]
+
+		avg = np.mean(tmp, axis=0)
+		np.savetxt(name, avg)
+
+
+def StandardDeviations(
+	file = 'XDATCAR', 
+	time_ini = 1, time_fin = 1000, 
+	save_avg = False, save_time = False, 
+	atom_list = [37, 148, 37]
+	):
+	
+	'''
+	time_ini/time_fin: including, start 1
+	time_ini is standard time. 
+	'''
+
+	xdatcar, lattice, atoms = CD.ReadXDATCAR(time_ini = time_ini, time_fin = time_fin,)
+	#xdatcar.shape = (times, atoms, 3)
+
+	pos_new = np.zeros([time_fin-time_ini+1, atoms, 3])
+	#pos_new.shape = (times, atoms, 3) 
+
+	sd_atoms = []; sd_atoms_time = np.zeros([atoms, time_fin-time_ini+1])
+
+	for atom in range(atoms):
+
+		pos_atom = np.array([xdatcar[time,atom,:] for time in range(time_fin - time_ini + 1)])
+		#pos_atom.shape = (times, atom)
+
+		pos_new[0, atom, :] = pos_atom[0, :]
+		
+		for time in range(1, time_fin - time_ini + 1):
+		 	pos_tmp = np.stack([pos_atom[time], pos_atom[0]])
+		 	#return Atom1(pos_atom[time])
+		 	pos_new[time, atom, :] = CD.Distance(pos=pos_tmp, print_='F', latt=lattice,
+		 		                          Atom1=1, Atom2=2)[1]
+
+		pos_time_atom = pos_new[:, atom, :]
+		#pos_time_atom.shape = (times, 3)
+		
+		avg_pos = np.mean(pos_time_atom, axis=0)
+		#avg_pos.shape = (1, 3)
+
+		distance_ = (pos_time_atom - avg_pos).dot(lattice)
+		distance = np.linalg.norm(distance_, axis=1)
+		#distance.shape = (times, )
+
+		# print(distance.shape)
+		# print('------------------')
+
+		sd_atoms.append(np.mean(distance))
+
+		if save_avg:
+			with open('SD_atoms.txt', 'a+') as f:
+				f.writelines(str(np.mean(distance))+'\n')
+
+		if save_time:
+			sd_atoms_time[atom, :] = distance
+
+	if save_time and len(atom_list) != 0:
+		SaveOut_time(atom_list = atom_list, sd_atoms_time = sd_atoms_time)
+
+	if len(atom_list) != 0:
+		PrintOut_avg(atom_list = atom_list, sd_atoms = sd_atoms)
+
+def main():
+	if SD_fun['TorF']:
+		StandardDeviations(file = 'XDATCAR',
+						   time_ini = SD_fun['time_ini'], time_fin = SD_fun['time_fin'],
+						   save_avg = SD_fun['save_avg'], save_time = SD_fun['save_time'],
+					       atom_list = SD_fun['atom_list'])
+
+
+if __name__ == '__main__':
+	main()
+
